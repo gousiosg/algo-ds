94ce150257f5020b2c34a908e419baa8d978827f
Daan Middendorp <github-d@landgenoot.com>
2017-03-19 17:44:25 +0100

    Some functions added to node class + pylint improvements

--- assignments/week 2/Solution.py
+++ assignments/week 2/Solution.py
@@ -1,8 +1,17 @@
 """Python implementation of doubly linked list"""
 class Node(object):
     """Doubly linked node which stores an object"""
-    def __init__(self, item, nextNode, previousNode):
-        self.item = item
-        self.next = nextNode
-        self.previous = previousNode
-        
+    def __init__(self, element, next_node, previous_node):
+        self.element = element
+        self.next_node = next_node
+        self.previous_node = previous_node
+    def get_element(self):
+        """Returns the element stored in this node"""
+        return self.element
+    def get_previous(self):
+        """Returns the previous linked node"""
+        return self.previous_node
+    def get_next(self):
+        """Returns the next linked node"""
+        return self.next_node
+    
