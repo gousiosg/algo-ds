4b8dc5915b525eee8d26a048dd884c3cf932ac43
Daan Middendorp <github-d@landgenoot.com>
2017-03-24 20:49:22 +0100

    Modify test for compatibility with Weblab

--- assignments/week 2/Test.py
+++ assignments/week 2/Test.py
@@ -7,10 +7,12 @@ from Solution import DoublyLinkedList, Node
 class TestNodeMethods(unittest.TestCase):
     """Test the behavior of the Node object"""
 
-    def test_size(self):
-        """Test size of DLL"""
+    def weblab_test(self):
+        """The content of this function can be copied to Weblab"""
+        test = self
+        
         list1 = DoublyLinkedList()
-        self.assertEqual(list1.size(), 0)
+        test.assertEquals(list1.size(), 0)
 
 if __name__ == '__main__':
     unittest.main()
